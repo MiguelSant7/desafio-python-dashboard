9349c316a807bf751c627970afd03ad31a0e7b78
MiguelSant7 <154387682+MiguelSant7@users.noreply.github.com>
2024-06-05 18:28:03 -0300

    Update brasil_dashboard.py

--- brasil_dashboard.py
+++ brasil_dashboard.py
@@ -42,13 +42,13 @@ filtered_UF = df[df['UF'] == option]
 
 ## Agregação data para uso
 aggregated_data = df.groupby("UF").agg({
-    "Capital": "first",               # Capital
-    "Population": "sum",            # Soma da População total
-    "Area": "sum",                  # Soma da área total
-    "Demographic Density": "mean",  # Média da densidade demográfica
-    "Cities count": "sum",          # Soma do número de cidades
-    "GDP": "mean",                  # Média do PIB
-    "GDP rate": "mean"              # Média do Crescimento do PIB
+    "Capital": "first",               
+    "Population": "sum",           
+    "Area": "sum",                  
+    "Demographic Density": "mean",  
+    "Cities count": "sum",          
+    "GDP": "mean",                  
+    "GDP rate": "mean"             
 }).reset_index()
 
 
@@ -71,30 +71,48 @@ pib = col_GDP.metric("PIB",
 
 #____________ Gráficos ____________#
 
-### Gráfico 1(fig_bar1)
+### Gráfico 1(fig_bar1) ###
 fig_bar1 = px.bar(
     filtered_UF, x='UF',y=['Population', 'Area'], barmode='group', title=f"Comparação de População por área do Estado {option}")
+# Definição de cor do gráfico 1
+fig_bar1.update_layout(plot_bgcolor='Black', paper_bgcolor='Black', font=dict(color='white'), xaxis=dict(showgrid=False, zeroline=False),
+    yaxis=dict(showgrid=True, zeroline=False))
 
-### Gráfico de Pizza(DataFrame "artificial"(Criado com base em dados pesquisados))
+
+### Gráfico de Pizza(DataFrame "artificial"(Criado com base em dados pesquisados)) ###
 data_pizza = {'Categoria': ['Pobreza', 'Extrema Pobreza', 'Vulnerabilidade'],'Porcentagem': [0.04, 0.19, 0.23]}
 df_pizza   = pd.DataFrame(data_pizza)
 fig_pie    = px.pie(df_pizza, values='Porcentagem', names='Categoria', title="Distribuição de Pobreza, Extrema Pobreza e Vulnerabilidade(Ano)",
-                 color_discrete_sequence=['#BD2A2E', '#F24607', '#EAF205']) # Vaiável que armazena o gráfico de pizza
+                 color_discrete_sequence=['#BD2A2E', '#F24607', '#EAF205'])
+# Definição de cor do Gráfico de pizza
+fig_pie.update_layout(plot_bgcolor='black',paper_bgcolor='black',font=dict(color='white'),)
 
-### Gráfico 2(fig_bar2), Uso do plotly.graph_objs
+
+### Gráfico 2(fig_bar2), Uso do plotly.graph_objs ###
 data     = [go.Bar(x=['PIB', 'Pobreza'], y=filtered_UF[['GDP rate', 'Poverty']].iloc[0].values, marker=dict(color=['#49D907', '#D90404']))]
-#Layout do gráfico
+# Layout do gráfico
 layout   = go.Layout(title=f"Comparação de PIB e Pobreza para {option}", xaxis=dict(title='Indicadores'), yaxis=dict(title='Valores')) 
-#Criação do Objeto  
-fig_bar2 = go.Figure(data=data, layout=layout)                           
+# Criação do Objeto  
+fig_bar2 = go.Figure(data=data, layout=layout)
+# Definição de cores do Gráfico de barras
+fig_bar2.update_layout(plot_bgcolor='black', paper_bgcolor='black', font=dict(color='white'), xaxis=dict(showgrid=False, zeroline=False),
+    yaxis=dict(showgrid=True, zeroline=False)) 
+                          
 
-### Gráfico Linhas
+### Gráfico Linhas ###
 fig_line = px.line(df_ind, x='referencia', y=['pobreza', 'extrema_pobreza', 'familias_pobreza', 'familias_extrema_pobreza'], 
                    title='Evolução da Pobreza e Extrema Pobreza', color_discrete_sequence=['#04D924', '#0903A6', '#F2B705', '#D9042B'])
+# Definição de cores do Gráfico de Linha
+fig_line.update_layout(plot_bgcolor='black',paper_bgcolor='black',font=dict(color='white'),xaxis=dict(showgrid=False, zeroline=False),
+    yaxis=dict(showgrid=True, zeroline=False))
 
-## Mapa Populacional
+
+### Mapa Populacional ###
 fig_map  = px.scatter_geo(df, lat='Latitude', lon='Longitude', hover_name='Capital', text='Capital',projection="natural earth",
-                         size='Population', size_max=20, color_continuous_scale='Viridis')
+                         size='Population', size_max=20, color='Region', color_continuous_scale='Viridis')
+# Definindo cores do mapa Populacional
+fig_map.update_layout(geo=dict(showland=True, landcolor="green",showocean=True,oceancolor="blue",bgcolor='black',),
+    plot_bgcolor='black',paper_bgcolor='black',font=dict(color='black'),legend_title_text='Região',coloraxis_colorbar=dict(title="Região"))
 
 
 #____________ Exibindo Conteúdo ____________#
@@ -104,11 +122,11 @@ st.subheader("Informações")
 col_figbar1, col_figpie = st.columns(2) 
 
 col_figbar1.plotly_chart(fig_bar1)        #Line 74
-col_figpie.plotly_chart(fig_pie)          #Line 78
+col_figpie.plotly_chart(fig_pie)          #Line 82
 
 col_figbar2, col_figline = st.columns(2)   
-col_figbar2.plotly_chart(fig_bar2)        #Line 84
-col_figline.plotly_chart(fig_line)        #Line 91
+col_figbar2.plotly_chart(fig_bar2)        #Line 91
+col_figline.plotly_chart(fig_line)        #Line 102
 
 # Exibição das Informações Gerais
 st.subheader("Informações Gerais")
@@ -117,6 +135,6 @@ st.write(filtered_UF)                     #Line 39
 # Exibição do Mapa e da Descrição
 st.subheader("Mapa do Brasil com as capitais dos Estados")
 col_fig_map, col_txt = st.columns([1, 1])
-col_fig_map.plotly_chart(fig_map)         #Line 95               
+col_fig_map.plotly_chart(fig_map)         #Line 110               
 col_txt.write("Esse mapa contém as informações populacionais de todas as capitais brasileiras, além de indicar População, \
               Latitude e Longitude de cada Estado brasileiro, para analisar o conteúdo aproxime-se do mapa do Brasil através do zoom.")
