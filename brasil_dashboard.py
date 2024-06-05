98ebedf04f650a23dfe54cc67a53522d65f1bdf6
MiguelSant7 <154387682+MiguelSant7@users.noreply.github.com>
2024-06-05 11:34:11 -0300

    Update brasil_dashboard.py

--- brasil_dashboard.py
+++ brasil_dashboard.py
@@ -71,7 +71,7 @@ pib = col_GDP.metric("PIB",
 
 #____________ Gráficos ____________#
 
-### Gráfico 1
+### Gráfico 1(fig_bar1)
 fig_bar1 = px.bar(
     filtered_UF, x='UF',y=['Population', 'Area'], barmode='group', title=f"Comparação de População por área do Estado {option}")
 
@@ -81,7 +81,7 @@ df_pizza   = pd.DataFrame(data_pizza)
 fig_pie    = px.pie(df_pizza, values='Porcentagem', names='Categoria', title="Distribuição de Pobreza, Extrema Pobreza e Vulnerabilidade(Ano)",
                  color_discrete_sequence=['#BD2A2E', '#F24607', '#EAF205']) # Vaiável que armazena o gráfico de pizza
 
-### Gráfico 2(Uso do plotly.graph_objs)
+### Gráfico 2(fig_bar2), Uso do plotly.graph_objs
 data     = [go.Bar(x=['PIB', 'Pobreza'], y=filtered_UF[['GDP rate', 'Poverty']].iloc[0].values, marker=dict(color=['#49D907', '#D90404']))]
 #Layout do gráfico
 layout   = go.Layout(title=f"Comparação de PIB e Pobreza para {option}", xaxis=dict(title='Indicadores'), yaxis=dict(title='Valores')) 
@@ -97,26 +97,26 @@ fig_map  = px.scatter_geo(df, lat='Latitude', lon='Longitude', hover_name='Capit
                          size='Population', size_max=20, color_continuous_scale='Viridis')
 
 
-###### Exibição de Todo Conteúdo
+#____________ Exibindo Conteúdo ____________#
 
 # Exibição do Primeiro e Segundo Gráfico
 st.subheader("Informações")
 col_figbar1, col_figpie = st.columns(2) 
 
-col_figbar1.plotly_chart(fig_bar1)        #Line 73
-col_figpie.plotly_chart(fig_pie)          #Line 77
+col_figbar1.plotly_chart(fig_bar1)        #Line 74
+col_figpie.plotly_chart(fig_pie)          #Line 78
 
 col_figbar2, col_figline = st.columns(2)   
-col_figbar2.plotly_chart(fig_bar2)        #Line 83
-col_figline.plotly_chart(fig_line)        #Line 90
+col_figbar2.plotly_chart(fig_bar2)        #Line 84
+col_figline.plotly_chart(fig_line)        #Line 91
 
 # Exibição das Informações Gerais
 st.subheader("Informações Gerais")
-st.write(filtered_UF)                     #Line 38
+st.write(filtered_UF)                     #Line 39
 
 # Exibição do Mapa e da Descrição
 st.subheader("Mapa do Brasil com as capitais dos Estados")
 col_fig_map, col_txt = st.columns([1, 1])
-col_fig_map.plotly_chart(fig_map)         #Line 94               
+col_fig_map.plotly_chart(fig_map)         #Line 95               
 col_txt.write("Esse mapa contém as informações populacionais de todas as capitais brasileiras, além de indicar População, \
               Latitude e Longitude de cada Estado brasileiro, para analisar o conteúdo aproxime-se do mapa do Brasil através do zoom.")
